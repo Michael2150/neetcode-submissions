224caf937fb12e3e8b840f4ba9e03543270fd854
Michael Gerber <52236517+Michael2150@users.noreply.github.com>
2026-07-09 17:16:51 +0100

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        start_par = ["{", "[", "("]
+        end_par = ["}", "]", ")"]
+        parenths = [l for l in s if l in start_par or l in end_par]
+
+        counts = {}
+
+        for x in parenths:  # Check the string
+            if x in start_par:  # Check if the char is a start_par
+                if x in counts:
+                    counts[x] += 1
+                else:
+                    counts[x] = 1
+
+            else:  # the char is an end_par
+                i = end_par.index(x)
+                strt_eq = start_par[i]
+
+                # See if we have an equivalent start_par available
+                if len(counts) == 0:
+                    return False
+
+                counts[strt_eq] -= 1
+                if counts[strt_eq] == 0:
+                    counts.pop(strt_eq)
+
+        print(counts)
+
+        return len(counts) == 0
